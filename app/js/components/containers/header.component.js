463ce8ec225fe53c5c0675faf054184611d58aec
Nathan Foreman <Nforeman@imodules.com>
2017-12-20 13:46:29 -0600

    Search funcitonallity added. Now just the JSX...

--- app/js/components/containers/header.component.js
+++ app/js/components/containers/header.component.js
@@ -1,58 +1,79 @@
-import React from 'react'; 
-import SearchResults from './searchResults.component';
+import React from "react";
+import SearchResults from "./searchResults.component";
 
-export default class Header extends React.Component { 
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: ""
+    };
+    this.updateQuery = this.updateQuery.bind(this);
+  }
+  componentWillUpdate() {
 
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-            query: "",
+  updateQuery(event) {
+    if (this.props.data) {
+      let query = event.target.value;
+      let found = false;
+      let searchResults = this.props.data.filter(currentValue => {
+        // Variable Test List"
+        // patternName
+        // patternTags
+        // patternAuthor
+        // patternDescription
+
+        // console.log(currentValue);
+        let addReject = false;
+
+        if (currentValue.patternName.search(query) > -1) {
+          addReject = true;
+          console.log("pattern Name");
         }
-        this.updateQuery = this.updateQuery.bind(this);
-    }
-    componentWillUpdate() {
-    }
-
-    updateQuery(event) {
-        if (this.props.data) {
-
-        let query = event.target.value;
-        let found = false;
-        let searchResults = this.props.data.filter((currentValue) => {
-                // Variable Test List" 
-                // patternName 
-                // patternTags
-                // patternAuthor
-                // patternDescription
-                console.log("accumulator: ", accumulator);
-                console.log("currentValue: ", currentValue);
-                
-         });
-
+        if (currentValue.patternTags.search(query) > -1) {
+          addReject = true;
+          console.log("Pattern Tags");
+        }
+        if (currentValue.patternAuthor.search(query) > -1) {
+          addReject = true;
+          console.log("pattern Author");
+        }
+        if (currentValue.patternDescription.search(query) > -1) {
+          addReject = true;
+          console.log("pattern Description");
         }
 
-     } 
-
-    render(){
-
-        return (
-            <div className="container-fluid appHead">
-                <div className="logo">
-                    <img src="images/logo.png" /> 
-                </div>
-                <div className="utilButtons"> 
-                </div>
-                <div className="search">
-                    <input name="searchText" id="searchText" onChange={this.updateQuery} type="text" />
-                    <a href="#" className="searchButton" id="searchButton"> <i className="fa fa-search" aria-hidden="true"></i> </a>
-                </div>
-                <SearchResults />
-                
-            </div> 
-
-        );
-
+        return addReject;
+      });
+      if (searchResults) {
+        console.log("search Results: ", searchResults);
+      }
+      this.setState({ searchResults: searchResults });
     }
+  }
 
+  render() {
+    return (
+      <div className="container-fluid appHead">
+        <div className="logo">
+          <img src="images/logo.png" />
+        </div>
+        <div className="utilButtons" />
+        <div className="search">
+          <input
+            name="searchText"
+            id="searchText"
+            onChange={this.updateQuery}
+            type="text"
+          />
+          <a href="#" className="searchButton" id="searchButton">
+            {" "}
+            <i className="fa fa-search" aria-hidden="true" />{" "}
+          </a>
+        </div>
+        <SearchResults results={this.state.searchResults}  />
+      </div>
+    );
+  }
 }
