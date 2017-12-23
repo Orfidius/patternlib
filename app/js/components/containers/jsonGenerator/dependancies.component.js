6527b74bdfd6d12b314f86d1970a1587548892ec
Nathan Foreman <Nforeman@imodules.com>
2017-12-22 20:54:58 -0600

    Json refactor conties

--- app/js/components/containers/jsonGenerator/dependancies.component.js
+++ app/js/components/containers/jsonGenerator/dependancies.component.js
@@ -4,34 +4,36 @@ export default class Dependancies extends React.Component {
   constructor(props) {
     super(props);
     this.updateData = this.props.updateFormData;
-
   }
 
   render() {
     return (
       <div id="jsonDependancies" className="pane jsonDependancies">
-        <div className="inputgroup">
-          <label>Javascript</label>
-          <input
-            type="text"
-            id="patternJsDep"
-            name="patternJsDep"
-            onChange={this.updateData}
-          />
+        <div className="tab1" id="tab1" style={this.state.isMeta}>
+          <div className="inputgroup">
+            <label>Javascript</label>
+            <input
+              type="text"
+              id="patternJsDep"
+              name="patternJsDep"
+              onChange={this.updateData}
+            />
+          </div>
         </div>
-        <div className="inputgroup">
-          <label>Css</label>
-          <input
-            type="text"
-            id="patternCssDep"
-            name="patternCssDep"
-            onChange={this.updateData}
-          />
+        <div className="tab2" id="tab2" style={this.state.isMeta}>
+          <div className="inputgroup">
+            <label>Css</label>
+            <input
+              type="text"
+              id="patternCssDep"
+              name="patternCssDep"
+              onChange={this.updateData}
+            />
+          </div>
         </div>
         <div className="inputgroup">
           <p> Jquery and Bootstrap added by default </p>
         </div>
-
       </div>
     );
   }
